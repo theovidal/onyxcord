65e692a4f8cc4f173f994b024f7d87e679353ae6
Exybore <exybore@becauseofprog.fr>
2019-10-21 18:04:26 +0200

    refactor: reorganize and enrich bot
    Reorganized the bot so commands are parsed by a struct method. Also,
    enriched the bot with rich presence and better log messages.

--- lib/bot.go
+++ lib/bot.go
@@ -1,9 +1,17 @@
 package lib
 
-import "github.com/andersfylling/disgord"
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/andersfylling/disgord"
+)
 
 // Bot represents the Discord bot with its assets
 type Bot struct {
+	// The Discord client associated with the bot
+	Client *disgord.Client
 	// A list of all the commands available on the bot
 	Commands map[string]*Command
 	// The configuration of the bot, as defined in the corresponding file
@@ -12,4 +20,37 @@ type Bot struct {
 	Session *disgord.Session
 	// The profile of the bot
 	User *disgord.User
+}
+
+func (bot *Bot) OnCommand(session disgord.Session, context *disgord.MessageCreate) {
+	msg := context.Message
+	composition := strings.Split(msg.Content, " ")
+	if commandName := composition[0]; strings.HasPrefix(commandName, bot.Config.Bot.Prefix) {
+		command, exists := bot.Commands[strings.TrimPrefix(commandName, bot.Config.Bot.Prefix)]
+		if exists {
+			execution, success := command.Execute.(func(arguments []string, bot Bot, context *disgord.MessageCreate))
+			if success {
+				bot.Session = &session
+				arguments := composition[1:]
+				execution(arguments, *bot, context)
+			} else {
+				log.Fatalf("Unknown error while executing a command")
+			}
+		} else {
+			_, _ = msg.Respond(
+				session,
+				&disgord.Message{
+					Embeds: []*disgord.Embed{
+						{
+							Title: ":x: Erreur",
+							Description: fmt.Sprintf("La commande %s est inconnue."+
+								"\nFaites `%s@help` pour obtenir la liste des commandes disponibles.",
+								commandName, bot.Config.Bot.Prefix),
+							Color: bot.Config.Bot.Color,
+						},
+					},
+				},
+			)
+		}
+	}
 }
--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"path/filepath"
-	"strings"
-
 	"github.com/BecauseOfProg/boite-a-bois/commands"
 	"github.com/BecauseOfProg/boite-a-bois/lib"
+	"log"
+	"path/filepath"
 
 	"github.com/andersfylling/disgord"
 )
@@ -18,17 +16,26 @@ import (
  */
 
 func main() {
+	// Loading the configuration
 	config, err := lib.GetConfig(filepath.Base("./config.yml"))
 	if err != nil {
 		log.Fatalf("Error while getting the configuration : %v", err)
+		return
 	}
 
-	discord := disgord.New(&disgord.Config{
+	// Creating the bot profile
+	client := disgord.New(&disgord.Config{
 		BotToken: config.Bot.Token,
+		Presence: &disgord.UpdateStatusCommand{
+			Since:  nil,
+			Game:   nil,
+			Status: fmt.Sprintf("%shelp", config.Bot.Prefix),
+			AFK:    false,
+		},
 	})
-	defer discord.StayConnectedUntilInterrupted()
+	defer client.StayConnectedUntilInterrupted()
 
-	user, err := discord.GetUser(
+	user, err := client.GetUser(
 		disgord.NewSnowflake(
 			uint64(config.Bot.ID),
 		),
@@ -39,46 +46,18 @@ func main() {
 	}
 
 	bot := lib.Bot{
+		Client:   client,
 		Commands: commands.List,
 		Config:   &config,
 		User:     user,
 	}
 
 	// Listen for incoming messages and parse them as commands
-	discord.On(disgord.EvtMessageCreate, func(session disgord.Session, context *disgord.MessageCreate) {
-		msg := context.Message
-		composition := strings.Split(msg.Content, " ")
-		if commandName := composition[0]; strings.HasPrefix(commandName, config.Bot.Prefix) {
-			command, exists := bot.Commands[strings.TrimPrefix(commandName, config.Bot.Prefix)]
-			if exists {
-				execution, success := command.Execute.(func(arguments []string, bot lib.Bot, context *disgord.MessageCreate))
-				if success {
-					bot.Session = &session
-					arguments := composition[1:]
-					execution(arguments, bot, context)
-				} else {
-					log.Fatalf("Error while executing a command : %v", err)
-				}
-			} else {
-				_, _ = msg.Respond(
-					session,
-					&disgord.Message{
-						Embeds: []*disgord.Embed{
-							{
-								Title: ":x: Erreur",
-								Description: fmt.Sprintf("La commande %s est inconnue."+
-									"\nFaites `%s@help` pour obtenir la liste des commandes disponibles.",
-									commandName, config.Bot.Prefix),
-								Color: config.Bot.Color,
-							},
-						},
-					},
-				)
-			}
-		}
+	client.On(disgord.EvtMessageCreate, func(session disgord.Session, context *disgord.MessageCreate) {
+		bot.OnCommand(session, context)
 	})
 
-	discord.Ready(func() {
-		fmt.Println("Successfully logged in.")
+	client.Ready(func() {
+		log.Printf("Successfully logged in as %s#%d\n", bot.User.Username, bot.User.Discriminator)
 	})
 }
