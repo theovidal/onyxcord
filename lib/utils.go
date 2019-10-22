54bcb3d9c54115c953edeb17ee8a5675d1d7cd92
Exybore <exybore@becauseofprog.fr>
2019-10-22 18:22:49 +0200

    feat: add embed creation utility

---
+++ lib/utils.go
@@ -0,0 +1,27 @@
+package lib
+
+import (
+	"fmt"
+	"github.com/andersfylling/disgord"
+	"time"
+)
+
+// MakeEmbed returns a Discord embed with the style of the bot
+func MakeEmbed(config *Config, base *disgord.Embed) *disgord.Embed {
+	return &disgord.Embed{
+		Title:       base.Title,
+		Description: base.Description,
+		Timestamp:   disgord.Time{time.Now()},
+		Color:       config.Bot.Color,
+		Footer: &disgord.EmbedFooter{
+			Text:    fmt.Sprintf("%s v%s", config.Bot.Name, config.Dev.Version),
+			IconURL: config.Bot.Illustration,
+		},
+		Image:     base.Image,
+		Thumbnail: base.Thumbnail,
+		Video:     base.Video,
+		Provider:  base.Provider,
+		Author:    base.Author,
+		Fields:    base.Fields,
+	}
+}
