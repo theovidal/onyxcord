be574d38acf30adaa276e8e4272dee4b8efe0c81
Exybore <exybore@becauseofprog.fr>
2019-11-16 18:04:07 +0100

    refactor: reorganize bot architecture
    Reorganized the architecture of the bot by clearly separating parts into
    different methods.

--- lib/bot.go
+++ lib/bot.go
@@ -3,6 +3,7 @@ package lib
 import (
 	"fmt"
 	"log"
+	"path/filepath"
 	"strings"
 
 	"github.com/andersfylling/disgord"
@@ -22,54 +23,94 @@ type Bot struct {
 	User *disgord.User
 }
 
+// LoadBot creates a new instance of the Discord Bot
+func LoadBot(commands map[string]*Command) Bot {
+	// Loading the configuration
+	config, err := GetConfig(filepath.Base("./config.yml"))
+	if err != nil {
+		log.Panicf("Error while getting the configuration : %v", err)
+	}
+
+	// Creating the bot profile
+	client := disgord.New(disgord.Config{
+		BotToken: config.Bot.Token,
+		Presence: &disgord.UpdateStatusPayload{
+			Since:  nil,
+			Game:   nil,
+			Status: fmt.Sprintf("%shelp", config.Bot.Prefix),
+			AFK:    false,
+		},
+	})
+
+	// Creating the user profile
+	user, err := client.GetUser(
+		disgord.NewSnowflake(
+			uint64(config.Bot.ID),
+		),
+	)
+	if err != nil {
+		log.Panicf("Error while creating the user profile : %v", err)
+	}
+
+	return Bot{
+		Client:   client,
+		Commands: commands,
+		Config:   &config,
+		User:     user,
+	}
+}
+
+// OnCommand reacts to a newly-created message and treats it
 func (bot *Bot) OnCommand(session disgord.Session, context *disgord.MessageCreate) {
+	bot.Session = &session
 	msg := context.Message
 	composition := strings.Split(msg.Content, " ")
 	if commandName := composition[0]; strings.HasPrefix(commandName, bot.Config.Bot.Prefix) {
 		commandName = strings.TrimPrefix(commandName, bot.Config.Bot.Prefix)
 		command, exists := bot.Commands[commandName]
 		if exists {
-			commandFunction, success := command.Execute.(func(arguments []string, bot Bot, context *disgord.MessageCreate) (err error))
-			if success {
-				bot.Session = &session
-				arguments := composition[1:]
-				err := commandFunction(arguments, *bot, context)
-				if err != nil {
-					_, _ = msg.Respond(
-						session,
-						&disgord.Message{
-							Embeds: []*disgord.Embed{
-								MakeEmbed(bot.Config, &disgord.Embed{
-									Title: ":x: Erreur",
-									Description: fmt.Sprintf("**%v**\n\n"+
-										"N'hésitez-pas à contacter %s (%s) si vous pensez que c'est un bug !",
-										err, bot.Config.Dev.Maintainer.Name,
-										bot.Config.Dev.Maintainer.Link),
-								}),
-							},
-						},
-					)
-					log.Printf("command %s (executed by %s#%d) failed to execute : %v",
-						commandName, context.Message.Author.Username, context.Message.Author.Discriminator, err)
-				}
-			} else {
-				log.Panicf("Unknown error while parsing a command")
-			}
+			bot.ExecuteCommand(command, composition[1:], context)
 		} else {
-			_, _ = msg.Respond(
-				session,
-				&disgord.Message{
-					Embeds: []*disgord.Embed{
-						{
-							Title: ":x: Erreur",
-							Description: fmt.Sprintf("La commande %s est inconnue."+
-								"\nExécutez `%shelp` pour obtenir la liste des commandes disponibles.",
-								commandName, bot.Config.Bot.Prefix),
-							Color: bot.Config.Bot.Color,
-						},
-					},
+			_, _ = context.Message.Reply(
+				bot.Client,
+				&disgord.CreateMessageParams{
+					Embed: MakeEmbed(bot.Config, &disgord.Embed{
+						Title: fmt.Sprintf("La commande %s est inconnue", commandName),
+						Description: fmt.Sprintf(
+							"\nExécutez `%shelp` pour obtenir la liste des commandes disponibles.",
+							bot.Config.Bot.Prefix),
+						Color: bot.Config.Bot.Color,
+					}),
 				},
 			)
 		}
+	}
+}
+
+// ExecuteCommand executes the command parsed in the OnCommand function
+func (bot *Bot) ExecuteCommand(command *Command, arguments []string, context *disgord.MessageCreate) {
+	commandFunction, success :=
+		command.Execute.(func(arguments []string, bot Bot, context *disgord.MessageCreate) (err error))
+
+	if success {
+		err := commandFunction(arguments, *bot, context)
+		if err != nil {
+			_, _ = context.Message.Reply(
+				bot.Client,
+				&disgord.CreateMessageParams{
+					Embed: MakeEmbed(bot.Config, &disgord.Embed{
+						Title: ":x: Erreur",
+						Description: fmt.Sprintf("**%v**\n\n"+
+							"N'hésitez-pas à contacter %s (%s) si vous pensez que c'est un bug !",
+							err, bot.Config.Dev.Maintainer.Name,
+							bot.Config.Dev.Maintainer.Link),
+					}),
+				},
+			)
+			log.Printf("Command %s (executed by %s#%d) failed to execute : %v",
+				command.Usage, context.Message.Author.Username, context.Message.Author.Discriminator, err)
+		}
+	} else {
+		log.Panicf("Unknown error while parsing a command")
 	}
 }
--- main.go
+++ main.go
@@ -1,61 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
 	"github.com/BecauseOfProg/boite-a-bois/commands"
 	"github.com/BecauseOfProg/boite-a-bois/lib"
-	"log"
-	"path/filepath"
-
 	"github.com/andersfylling/disgord"
 )
 
 /*
  * This file is the entry point of the bot, where it's setup.
- * For the moment is just a bunch of code that'll, in the future, be split between multiple files.
  */
 
 func main() {
-	// Loading the configuration
-	config, err := lib.GetConfig(filepath.Base("./config.yml"))
-	if err != nil {
-		log.Panicf("Error while getting the configuration : %v", err)
-	}
-
-	// Creating the bot profile
-	client := disgord.New(&disgord.Config{
-		BotToken: config.Bot.Token,
-		Presence: &disgord.UpdateStatusCommand{
-			Since:  nil,
-			Game:   nil,
-			Status: fmt.Sprintf("%shelp", config.Bot.Prefix),
-			AFK:    false,
-		},
-	})
-	defer client.StayConnectedUntilInterrupted()
-
-	user, err := client.GetUser(
-		disgord.NewSnowflake(
-			uint64(config.Bot.ID),
-		),
-	)
-	if err != nil {
-		log.Panicf("Error while creating the user profile : %v", err)
-	}
-
-	bot := lib.Bot{
-		Client:   client,
-		Commands: commands.List,
-		Config:   &config,
-		User:     user,
-	}
+	bot := lib.LoadBot(commands.List)
+	defer bot.Client.StayConnectedUntilInterrupted()
 
 	// Listen for incoming messages and parse them as commands
-	client.On(disgord.EvtMessageCreate, func(session disgord.Session, context *disgord.MessageCreate) {
+	bot.Client.On(disgord.EvtMessageCreate, func(session disgord.Session, context *disgord.MessageCreate) {
 		bot.OnCommand(session, context)
 	})
 
-	client.Ready(func() {
-		log.Printf("Successfully logged in as %s#%d\n", bot.User.Username, bot.User.Discriminator)
+	// Print to the console when the bot is ready
+	bot.Client.Ready(func() {
+		log.Printf("Logged in as %s#%d\n", bot.User.Username, bot.User.Discriminator)
 	})
 }
