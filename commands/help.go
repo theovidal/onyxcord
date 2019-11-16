5c8954ad897629a2a58380629992bf32cf42b4b5
Exybore <exybore@becauseofprog.fr>
2019-11-16 18:14:47 +0100

    feat: add help command

---
+++ commands/help.go
@@ -0,0 +1,44 @@
+package commands
+
+import (
+	"fmt"
+	"github.com/BecauseOfProg/boite-a-bois/lib"
+	"github.com/andersfylling/disgord"
+)
+
+var help = lib.Command{
+	Description: "Obtenir de l'aide sur les commandes du robot",
+	Usage:       "help",
+	Category:    "utilities",
+	Listen:      []string{"public", "private"},
+	Execute: func(arguments []string, bot lib.Bot, context *disgord.MessageCreate) (err error) {
+		commandsList := make(map[string]string)
+		for name, command := range bot.Commands {
+			if command.Show {
+				continue
+			} else {
+				commandsList[command.Category] += command.Prettify(name, bot.Config.Bot.Prefix) + "\n"
+			}
+		}
+
+		var fullMessage disgord.Embed
+		for categoryName, commands := range commandsList {
+			category := bot.Config.Categories[categoryName]
+			fullMessage.Fields = append(fullMessage.Fields, &disgord.EmbedField{
+				Name:  fmt.Sprintf(":%s: %s", category.Emoji, category.Name),
+				Value: commands,
+			})
+		}
+
+		_, _ = context.Message.Reply(
+			bot.Client,
+			&disgord.CreateMessageParams{
+				Embed: lib.MakeEmbed(
+					bot.Config,
+					&fullMessage,
+				),
+			},
+		)
+		return
+	},
+}
