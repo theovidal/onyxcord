c69372ced3009183009cc3a4f0c73280e34e44e8
Théo Vidal <exybore@becauseofprog.fr>
2020-10-04 22:02:42 +0200

    style: add more logs and cool ascii-art

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/theovidal/onyxcord/commands"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/theovidal/onyxcord/bots"
+	"github.com/theovidal/onyxcord/commands"
 	"github.com/theovidal/onyxcord/lib"
 
 	_ "go.mongodb.org/mongo-driver/bson"
@@ -21,18 +21,21 @@ import (
  */
 
 func main() {
+	fmt.Println(" ________  ________       ___    ___ ___    ___ \n|\\   __  \\|\\   ___  \\    |\\  \\  /  /|\\  \\  /  /|\n\\ \\  \\|\\  \\ \\  \\\\ \\  \\   \\ \\  \\/  / | \\  \\/  / /\n \\ \\  \\\\\\  \\ \\  \\\\ \\  \\   \\ \\    / / \\ \\    / / \n  \\ \\  \\\\\\  \\ \\  \\\\ \\  \\   \\/  /  /   /     \\/  \n   \\ \\_______\\ \\__\\\\ \\__\\__/  / /    /  /\\   \\  \n    \\|_______|\\|__| \\|__|\\___/ /    /__/ /\\ __\\ \n                        \\|___|/     |__|/ \\|__| ")
+
+	log.Println("Opening database...")
 	lib.OpenDatabase()
 
-	for bot := range bots.Bots {
-		bot.Commands["help"] = &commands.Help
+	log.Println("Registering bots...")
+	for _, bot := range bots.Bots {
+		bot.Commands["help"] = commands.Help()
+
 		// Open a websocket connection to Discord and begin listening.
 		err := bot.Client.Open()
 		if err != nil {
-			fmt.Println("error opening connection with bot: ", err)
-			return
+			log.Fatalf("Error opening connection with bot %s: %s\n", bot.Name, err)
 		}
-		fmt.Println(bot)
-		log.Printf("%s#%s logged in\n", bot.User.Username, bot.User.Discriminator)
+		log.Printf("%s#%s logged in!\n", bot.User.Username, bot.User.Discriminator)
 	}
 
 	// Wait here until CTRL-C or other term signal is received.
@@ -40,13 +43,13 @@ func main() {
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 
-	log.Println("Closing down...")
+	log.Println("Closing down bots...")
 
-	for bot := range bots.Bots {
+	for _, bot := range bots.Bots {
 		// Cleanly close down sessions
 		bot.Client.Close()
 		log.Printf("%s#%s disconnected\n", bot.User.Username, bot.User.Discriminator)
 	}
 	lib.Db.Disconnect(context.Background())
-	log.Println("Goodbye")
+	log.Println("Goodbye!")
 }
